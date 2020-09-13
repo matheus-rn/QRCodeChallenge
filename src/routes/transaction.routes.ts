07eeb06957ff3cab41aa6d29ffb9ef25eadce13e
mrodrigues <matheus.1nascimento58@gmail.com>
2020-09-12 23:54:55 -0300

    feature: encoding qrcode

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import { hash } from 'bcryptjs';
 
 const transactionRouter = Router();
 
 transactionRouter.post('/', async (request, response) => {
+  const { body } = request;
   try {
-    // TODO
+    const qrCodeString = await hash(JSON.stringify(body), 10);
+
+    return response.json({ qrCodeString });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
